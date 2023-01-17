defeb0be2f2278a44920cfdeee6399c9aa593ac9
Dallas Larsen <dallas@thegalacticdesignbureau.com>
2023-01-17 08:26:25 -0800

    Clean home up

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ posts}) {
+  
   const jsxPosts = posts.map((post) => {
-   
     return <div key={post.id} className={styles.card}><h4>{post.title.rendered}</h4></div>;
   });
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +18,7 @@ export default function Home({ posts}) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wordpress headless/graphql next.js UI test</a>
         </h1>
 
         <p className={styles.description}>
@@ -26,37 +27,8 @@ export default function Home({ posts}) {
         </p>
 
         <div className={styles.grid}>
-           <h2 className="pb-3">Our blog posts</h2>
+           <h2 className="pb-3">Galactic Pages</h2>
             {jsxPosts}
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -75,9 +47,11 @@ export default function Home({ posts}) {
     </div>
   );
 }
+
+const BASE_URL = 'https://thegalacticdesignbureau.com/wp-json/wp/v2';
+
 export async function getStaticProps({ params }) {
   const posts = await getPosts();
-
   return {
     props: {
       posts,
@@ -85,7 +59,6 @@ export async function getStaticProps({ params }) {
     revalidate: 10, // In seconds
   };
 }
-const BASE_URL = 'https://thegalacticdesignbureau.com/wp-json/wp/v2';
 
 export async function getPosts() {
   const postsRes = await fetch(BASE_URL + '/pages?_embed');
